26ed51e4d7faa4dcb47f071aa8e3dd8f7f63e37c
melniknikolay <unclenikoboss@gmail.com>
2021-09-22 13:06:55 +0300

    Add the Add Place Screen and Image Input

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './add_place_screen.dart';
+
 class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,6 +10,14 @@ class PlacesListScreen extends StatelessWidget {
         title: Text(
           'Your Places',
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          ),
+        ],
       ),
       body: Center(
         child: CircularProgressIndicator(),
